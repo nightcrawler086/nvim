4ece5c4b08b9213e8fd3df62a4c2dac18f2eafbb
Brian Hall <brian.hall4@gmail.com>
2023-05-01 22:47:47 -0400

    shortcut for a split terminal

--- lua/nightcrawler/remap.lua
+++ lua/nightcrawler/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- new horizontal terminal
+vim.keymap.set("n", "<leader>t", "<C-w>s :term<CR>i")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
